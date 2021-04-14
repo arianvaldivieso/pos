094aed05ab0b547ae7ced154c47cda0547967577
Arian Valdivieso <arianvaldivieso@gmail.com>
2021-04-14 01:16:15 -0400

    feat: archivos de base de datos

---
+++ application/config/database.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default']['hostname'] = 'sql';
+$db['default']['dsn'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'pos';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbprefix'] = 'phppos_';
+$db['default']['pconnect'] = FALSE;
+$db['default']['db_debug'] = FALSE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_unicode_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['encrypt'] = FALSE;
+$db['default']['compress'] = FALSE;
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
+$db['default']['failover'] = array();
+$db['default']['save_queries'] = FALSE;
